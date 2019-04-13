96c7a28a9c3784a2cabf7ec59440c744484a6c4a
aedit <udisen97@gmail.com>
2019-04-13 19:08:36 +0530

    Heroku

--- mongo.py
+++ mongo.py
@@ -9,7 +9,7 @@ from flask_jwt_extended import JWTManager, create_access_token
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'users'
-app.config['MONGO_URI'] = 'mongodb+srv://admin:admin@hovergames-inytk.gcp.mongodb.net/game'
+app.config['MONGO_URI'] = 'mongodb+srv://admin:admin@hovergames-inytk.gcp.mongodb.net/game?retryWrites=true'
 app.config['JWT_SECRET_KEY'] = 'secret'
 
 mongo = PyMongo(app)
